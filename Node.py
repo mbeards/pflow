bbd03322beaeee2632df5af0f8b795546c323ebe
Michael Beardsworth <michael.beardsworth@gmail.com>
2013-04-29 20:04:19 -0700

    updated topology

--- Node.py
+++ Node.py
@@ -1,12 +1,22 @@
 from SimPy.Simulation import *
 from Packet import *
 
+class Route:
+  def __init__(self, ip, prefix, link):
+    self.ip = ip
+    self.prefix = prefix
+    self.link = link
+
+
 class Node:
 
   def __init__(self, name):
     self.name = name
     self.generator = Generator(name="packetsource")
     self.generator.parent = self
+    self.link = None
+    self.ip = 0
+    self.netmask = 0
 
   def setlink(self,link):
     self.link = link
