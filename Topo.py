c70e0ea832a655374ce0715d01df3af304afbf82
Michael Beardsworth <michael.beardsworth@gmail.com>
2013-05-01 22:58:46 -0700

    map node IPs to link pointers

--- Topo.py
+++ Topo.py
@@ -4,14 +4,19 @@ from Node import *
 from Link import *
 from netaddr import *
 
+#Linktable is "srcipdstip":link
+linktable = {}
+
+
+      
+
 
 def setup_node(i):
   n = Node(name=("Node"+str(i)))
   n.setprefix(IPNetwork("10.0."+str(i)+"/24"))
   return n
 
-def generate_topology():
-  size = 10
+def generate_topology(size):
 
   #make nodes
   nodes = [setup_node(i) for i in range(size)]
@@ -29,6 +34,11 @@ def generate_topology():
     for neighbor in g[node].keys():
       #add a link to the neighbor
       link = Link(name=("Link"+str(node)+"-"+str(neighbor)), capacity=4) #need to vary link capacity, but don't worry yet
+
+      #map node IPs to link pointers
+      labelstr = str(nodes[node].prefix[1]) + str(nodes[neighbor].prefix[1])
+      linktable[labelstr] = link
+
       link.setup(nodes[neighbor], 25) #set propdelay and destination
       nodes[node].add_link(link) #hook link on to parent
 
@@ -37,6 +47,9 @@ def generate_topology():
     print nodes[node].list_links()
 
 
-generate_topology()
 
 
+
+generate_topology(254)
+
+print linktable
